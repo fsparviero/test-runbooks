8cfb3eec1dacdcd13a51cbfb51c6931debcf2f61
fsparviero <67952884+fsparviero@users.noreply.github.com>
2023-10-02 23:07:30 +0100

    Update starts-vms-preprod.ps1

--- starts-vms-preprod.ps1
+++ starts-vms-preprod.ps1
@@ -8,7 +8,7 @@ Connect-AzAccount -Identity
 write-host "Login complete"
 
 # Starting the all non prod vms 
-write-host "starting vms in the rg-US6122593-pdp-test resource group " 
+write-host "starting vms in the rg-bastion-vms-preprod resource group " 
 Get-AzVm -ResourceGroupName 'rg-bastion-vms-preprod' |Start-AzVM -nowait
 #checking on the vms status 
 write-host "Checking on the status of the virtual machines" 
