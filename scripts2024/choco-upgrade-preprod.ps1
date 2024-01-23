995537643ef14347c3d39ef1c4e94b826a3907f5
Sparviero, Francesco G <francesco.sparviero@optum.com>
2024-01-23 10:46:52 +0000

    new scripts 2024

---
+++ scripts2024/choco-upgrade-preprod.ps1
@@ -0,0 +1,43 @@
+# Import the modules
+Import-module 'az.accounts'
+Import-module 'az.compute'
+
+# Connect to Azure with the System Managed Identity
+Connect-AzAccount -Identity
+
+write-host "Login complete"
+
+$ResourceGroupName = "rg-bastion-vms-preprod"
+
+write-host "Getting vm's"
+$AZVMs = Get-Azvm -ResourceGroupName $ResourceGroupName
+ 
+ 
+#run local script on selected AZ running VM's
+Write-Host "Submitting command to run on Azure VM's" -foregroundcolor Yellow
+$AZVMs|ForEach-Object -Parallel {
+  Invoke-AzVMRunCommand -ResourceGroupName $_.ResourceGroupName -Name $_.Name -CommandId 'RunPowerShellScript' -ScriptString "choco.exe update all -y -i" -asjob | out-null
+}
+#check scripts complete
+ 
+ 
+$Monitoring = $AZVMs.count
+#Track events
+$TrackEvents = @()
+DO{
+  foreach($AZVM in $AZVMs){
+    Write-Host "Getting Status of VM $($AZVM.Name)" -foregroundcolor Yellow
+    $DisplayStatus = ($AZVM | Get-Azvm -status).Statuses[0].DisplayStatus
+    switch($DisplayStatus){
+    "Updating" {
+      Write-Host "$($AZVM.Name): status $($DisplayStatus)" -foregroundcolor Red
+    }
+    "Provisioning succeeded" {
+      Write-Host "$($AZVM.Name): status $($DisplayStatus)" -foregroundcolor Green
+      $Monitoring = ($Monitoring - 1)
+    }
+    default {Write-Host "$($AZVM.Name): status $($DisplayStatus)" -foregroundcolor Yellow}
+    }
+  }
+  start-sleep -seconds 3
+}While($Monitoring -gt 0)
---
+++ scripts2024/starts-vm-qa.ps1
@@ -0,0 +1,57 @@
+# Import the modules
+Import-module 'az.accounts'
+Import-module 'az.compute'
+
+# Connect to Azure with the System Managed Identity
+Connect-AzAccount -Identity
+
+write-host "Login complete"
+
+# Starting the all non prod vms 
+#get's vms which are not running
+
+$VMsToMonitor = (Get-Azvm -status -ResourceGroupName "rg-bastion-vms-qa") | Where-Object{$_.PowerState -notlike "VM running"}
+
+#issues start to non running vms
+
+$VMsToMonitor | Where-Object{$_.PowerState -notlike "VM running"}|Start-AzVM -nowait
+
+$Monitoring = $VMsToMonitor.count
+
+#monitor vms until started
+
+DO{
+
+  foreach($AZVM in $VMsToMonitor){
+
+	  Write-Host "Getting Status of VM $($AZVM.Name)" -foregroundcolor Yellow
+
+	  $PowerState = ($AZVM | Get-Azvm -status).Statuses[1].DisplayStatus
+
+	  switch($PowerState){
+
+		"VM running" {
+
+			Write-Host "$($AZVM.Name): status $($PowerState)" -foregroundcolor Green
+
+			$Monitoring = ($Monitoring - 1)				
+
+		}
+
+		Default {
+
+			Write-Host "$($AZVM.Name): status $($PowerState)" -foregroundcolor Red
+
+		}
+
+	  }
+
+  }
+
+    Write-Host "Wait 10 seconds" -foregroundcolor Yellow
+
+	start-sleep -seconds 10
+
+}While($Monitoring -gt 0)
+
+
